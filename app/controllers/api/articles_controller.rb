a1a8fa7ed05d25f2b3d5d64ce21dd294fb9ad98e
Peter Jang <peter.x.jang@gmail.com>
2019-07-14 11:54:57 -0500

    Add example news api request action

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,2 +1,7 @@
 class Api::ArticlesController < ApplicationController
+  def index
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=#{ENV["NEWS_API_KEY"]}")
+    articles = response.parse
+    render json: articles
+  end
 end
